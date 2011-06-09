133807f9aa5d286770b0411b749965f3c0ff5419
finrist <finrist@gmail.com>
2011-06-09 22:15:15 +0200

    time and renderer and stuff

--- src/se/nederlag/pyzzlix/Pyzzlix.java
+++ src/se/nederlag/pyzzlix/Pyzzlix.java
@@ -2,19 +2,10 @@ package se.nederlag.pyzzlix;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Pyzzlix implements ApplicationListener  {
-	static final int SPRITES = 400000;
 	static final int LOGICS_PER_SEC = 20;
 	
-	Texture image;
-	Sprite[] sprites;	
-	SpriteBatch batch;
-
 	int fpsCounter = 0;	
 
 	float time = 0.0f;
@@ -33,20 +24,10 @@ public class Pyzzlix implements ApplicationListener  {
 	}
 	
 	public void create() {	
-		image = new Texture(Gdx.files.internal("data/sprite.png")); 												  
-		
-		sprites = new Sprite[SPRITES];
-		for(int i = 0; i < SPRITES; i++) {
-			Sprite sprite = new Sprite(image);
-			sprite.setPosition((float)Math.random() * 800, (float)Math.random() * 600);
-			sprites[i] = sprite;
-		}
- 
-		batch = new SpriteBatch();
 	}
  
 	public void resize(int width, int height){
-		batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+		//batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
 	}
 	
 	public void render() {
@@ -78,20 +59,7 @@ public class Pyzzlix implements ApplicationListener  {
 			Renderer.INSTANCE.render(time - lastRenderTime);
 			lastRenderTime = time;
 			fpsCounter++;
-		}
-		
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
- 
-		float scale = (float)(Math.sin(elapsed*Math.PI)*Math.sin(elapsed*Math.PI));
- 
-		batch.begin();
-		batch.enableBlending();
-		for(int i = 0; i < SPRITES; i++) {				
-			sprites[i].setRotation(sprites[i].getRotation() + elapsed * 45);			
-			sprites[i].setScale(scale, scale);
-			sprites[i].draw(batch);			
-		}
-		batch.end();			
+		}		
 	}
  
 	public boolean needsGL20 () {
--- src/se/nederlag/pyzzlix/Renderer.java
+++ src/se/nederlag/pyzzlix/Renderer.java
@@ -1,9 +1,47 @@
 package se.nederlag.pyzzlix;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public enum Renderer {
 	INSTANCE;
+
+	static final int SPRITES = 400000;
+
+	Texture image;
+	Sprite[] sprites;	
+	SpriteBatch batch;
+	
+	private Renderer()
+	{
+		image = new Texture(Gdx.files.internal("data/sprite.png")); 												  
+		
+		sprites = new Sprite[SPRITES];
+		for(int i = 0; i < SPRITES; i++) {
+			Sprite sprite = new Sprite(image);
+			sprite.setPosition((float)Math.random() * 800, (float)Math.random() * 600);
+			sprites[i] = sprite;
+		}
+ 
+		batch = new SpriteBatch();		
+	}
 	
 	public void render(float deltatime)
 	{		
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		 
+		float scale = 1.0f - deltatime;		
+ 
+		batch.begin();
+		batch.enableBlending();
+		for(int i = 0; i < SPRITES; i++) {				
+			sprites[i].setRotation(sprites[i].getRotation() + deltatime * 45);			
+			sprites[i].setScale(scale, scale);
+			sprites[i].draw(batch);			
+		}
+		batch.end();			
 	}
 }
